a0d874170793fb585d2d6ab501a53362e1d7c276
Richie <richiecontrolsystems@outlook.com>
2023-10-28 15:46:26 +0100

    Backup - Home Saturday 15:30

--- src/App.js
+++ src/App.js
@@ -3,34 +3,20 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './features/header/Header';
 import { WatchList } from './features/watchList/WatchList';
 import { SearchResults } from './features/searchResults/SearchResults';
-import { Footer } from './features/footer/Footer';
-import { createTheme, ThemeProvider } from '@mui/material';
+import { Footer } from './features/footer/Footer'
 
 
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#253439'
-    },
-    secondary: {
-      main: '#ffff'
-    },
-  }
-})
 
 
 function App() {
   return (
     <>
-      <ThemeProvider theme={theme}> 
-        <Header />
-        <Routes>
-          <Route path="/" element={ <SearchResults /> } />
-          <Route path="/WatchList" element={ <WatchList /> } />
-        </Routes> 
-        <Footer />
-      </ThemeProvider>     
+      <Header />
+      <Routes>
+        <Route path="/" element={ <SearchResults /> } />
+        <Route path="/WatchList" element={ <WatchList /> } />
+      </Routes> 
+      <Footer />      
     </>
   );
 }
