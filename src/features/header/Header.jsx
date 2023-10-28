0ba418c60387fe779bd5d21467a11a07a32f8553
Richie <richiecontrolsystems@outlook.com>
2023-10-28 21:27:31 +0100

    Search field appearance fixed

--- src/features/header/Header.jsx
+++ src/features/header/Header.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setMovieSearchTerm } from '../searchResults/searchResultsSlice';
 import { setBadgeCount } from '../watchList/watchListSlice';
 import { fetchMoviesData } from '../searchResults/getSearchResults';
-import { styled } from '@mui/material/styles';
+import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -22,12 +22,46 @@ import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
 import Badge from '@mui/material/Badge';
 
 const Search = styled('div')(({ theme }) => ({
-  // ... your styles
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
 }));
 
-const StyledInputBase = styled(InputBase)((({ theme }) => ({
-  // ... your styles
-})));
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
 
 export default function Header() {
   const [searchTerm, setSearchTerm] = useState('');  
@@ -143,8 +177,8 @@ export default function Header() {
           </IconButton>
           </Link>
           <Search onChange={onSearchChanged}>
-            <IconButton size="large" aria-label="search" color="inherit" onClick={onSearchClicked}>
-              <SearchIcon />
+            <IconButton size="large" aria-label="search" color="inherit" onClick={onSearchClicked}>              
+              <SearchIcon />              
             </IconButton>
             <StyledInputBase
               placeholder="Search"
